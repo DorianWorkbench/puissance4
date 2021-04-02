b00936227100a14338b370ab576ad3e70272b1d5
DorianWorkbench <prhdorian@gmail.com>
2021-04-02 23:26:37 +0200

    ajoutAction

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@ import { Puissance4 } from "./src/Puissance4";
 
 let puissance4 = new Puissance4();
 
-puissance4.play(1,2);
-puissance4.play(1,3);
+puissance4.play(1, 2);
+puissance4.play(1, 3);
+puissance4.play(1, 4);
 
 puissance4.verifCoupHorizontal();
